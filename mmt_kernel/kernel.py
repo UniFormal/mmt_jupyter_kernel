a4f5c0dcea749bad27a9227998cb905bbd6c3852
Kai <kai@ubuntu.ubuntu-domain>
2018-04-12 15:11:10 +0200

    changed MMT Kernel to forward messages to the repl extension

--- mmt_kernel/kernel.py
+++ mmt_kernel/kernel.py
@@ -1,5 +1,4 @@
 import requests
-from . import mmtshell
 import sys
 from pexpect import replwrap
 
@@ -10,6 +9,8 @@ except ImportError:
 
 from ipykernel.kernelbase import Kernel
 
+MMT_SERVER_EXTENSION = 'repl'
+MMT_BASE_URL = 'http://localhost:9000'
 
 
 class MMTKernel(Kernel):
@@ -18,25 +19,49 @@ class MMTKernel(Kernel):
     language = 'mmt-action-script'
     language_version = '0.1'
     language_info = {
-        'name': 'Any text',
-        'mimetype': 'text/plain',
-        'file_extension': '.txt',
+    'name': 'Any text',
+    'mimetype': 'text/plain',
+    'file_extension': '.txt',
     }
     banner = "MMT kernel - running build command on MMT"
+    STARTUP = True
 
-    def do_execute(self, line, silent, store_history=True,
-                   user_expressions=None,
-                   allow_stdin=False):
-       if not silent:
-           stream_content = s.handleInputFromKernel(line)
-           res = self.send_response(self.iopub_socket, 'display_data',
-           stream_content)
 
-           return {'status': 'ok',
-               # The base class increments the execution count
-               'execution_count': self.execution_count,
-               'payload': [],
-               'user_expressions': {},
-           }
+    def __init__(self, **kwargs):
+        super(MMTKernel,self).__init__(**kwargs)
+        self.mmtsession = requests.Session()
+        self.adapter = requests.adapters.HTTPAdapter()
+        self.mmtsession.mount('https://', self.adapter)
+        self.mmtsession.mount('http://', self.adapter)
+        self.headers = {'content-type' : 'application/json',
+                        'content-encoding' : 'UTF-8',
+                        'mmtsession' : '12345xyz'}
 
-s = mmtshell.MMTShell()
+
+
+    def do_execute(self, line, silent, store_history=True,user_expressions=None,allow_stdin=False):
+
+
+        if(self.STARTUP):
+            out = requests.get(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION+'?start',data = None,headers = self.headers, stream = True)
+            self.STARTUP = False
+
+        binary_data = line.encode('UTF-8')
+
+        stream_content = {
+            'data': {
+                'text/html':  self.mmtsession.post(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION,data = binary_data,headers = self.headers, stream = True).text
+            },
+            'metadata': {},
+            'transient': {},
+        }
+
+        if not silent:
+            res = self.send_response(self.iopub_socket, 'display_data',stream_content)
+
+            return {'status': 'ok',
+            # The base class increments the execution count
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {},
+            }
