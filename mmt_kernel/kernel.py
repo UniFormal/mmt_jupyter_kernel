f3fa16656506aca804c4bc0c8c3cc5c74a19199f
Kai <kai@ubuntu.ubuntu-domain>
2018-04-26 13:02:35 +0200

    Updated MMT-Kernel to the new REPL-Extension

--- mmt_kernel/kernel.py
+++ mmt_kernel/kernel.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import os
+import json
 from pexpect import replwrap
 
 try:
@@ -14,6 +15,13 @@ MMT_SERVER_EXTENSION = 'repl'
 MMT_BASE_URL = os.environ.setdefault('MMT_BASE_URL', 'http://localhost:9000')
 
 
+class MMTError(Exception):
+    message = ''
+    def __init__(self,message,**kwargs):
+        Exception.__init__(self,message,**kwargs)
+        self.message = message
+
+
 class MMTKernel(Kernel):
     implementation = 'MMT'
     implementation_version = '1.2'
@@ -25,8 +33,7 @@ class MMTKernel(Kernel):
     'file_extension': '.txt',
     }
     banner = "MMT kernel - running build command on MMT"
-    STARTUP = True
-
+    debugprint = True
 
     def __init__(self, **kwargs):
         super(MMTKernel,self).__init__(**kwargs)
@@ -35,39 +42,83 @@ class MMTKernel(Kernel):
         self.mmtsession.mount('https://', self.adapter)
         self.mmtsession.mount('http://', self.adapter)
         self.headers = {'content-type' : 'application/json',
-                        'content-encoding' : 'UTF-8',
-                        'mmtsession' : '12345xyz'}
+                        'content-encoding' : 'UTF-8'}
+        try:
+            response_dict = self.mmtsession.post(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION+'?start',data = None,headers = self.headers, stream = True).json()
+            sessionheader = { 'X-REPL-Session' : response_dict['session'] }
+            self.headers = {**self.headers, **sessionheader}
+        except Exception as e:
+            pass
 
+    def do_execute(self, code, silent, store_history=True,user_expressions=None,allow_stdin=False):
+        try:
+            response_dict = self.handle_request(code)
+            message = response_dict['message']
+            if not response_dict['success']:
+                raise MMTError(message)
+        # for connection errors
+        except requests.exceptions.RequestException:
+            message = 'Unable to communicate with the MMT-Server'
 
+        # for internal MMT errors
+        except MMTError as e:
+            message = self.wrap_errors('An internal MMT Error occured:')
+            if self.debugprint:
+                message += """
+                <html>
+                <div id="stacktrace"  style="display:none;"> """+e.message+"""</div>
+                <input id="button" type="button" name="button" value="Show Stacktrace" onclick="toggle()" />
+                <script>
+                    function toggle() {
+                      	var elem = document.getElementById('button')
+                      	if(elem.value == "Show Stacktrace"){
+                        	elem.value = "Hide Stacktrace";
+                      		document.getElementById('stacktrace').style.display = "block";
+                      	}
+                        else {
+                        	elem.value = "Show Stacktrace";
+                        	document.getElementById('stacktrace').style.display = "none";
+                        }
+                  }
+                </script>
+                </html>"""
+        # for all the other errors
+        except Exception as e:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = self.get_stream_content(self.wrap_errors(template.format(type(e).__name__, e.args)))
+        finally:
+            if not silent:
+                # send the response to the frontend
+                self.send_response(self.iopub_socket, 'display_data',self.get_stream_content(message))
+                return {'status': 'ok',
+                # The base class increments the execution count
+                'payload' : [],
+                'execution_count': self.execution_count,
+                'user_expressions': {},
+                }
 
-    def do_execute(self, line, silent, store_history=True,user_expressions=None,allow_stdin=False):
+    # handles the requests to the MMT-Server
+    def handle_request(self,code):
+        binary_data = code.encode('UTF-8')
+        return self.mmtsession.post(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION,data = binary_data,headers = self.headers, stream = True).json()
 
-        if(self.STARTUP):
-            out = requests.get(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION+'?start',data = None,headers = self.headers, stream = True)
-            self.STARTUP = False
+    # marks errors red
+    def wrap_errors(self,error):
+        return '<p style="color:red;">'+error+'</p>'
 
-        binary_data = line.encode('UTF-8')
-
-        stream_content = {
-            'data': {
-                'text/html':  self.mmtsession.post(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION,data = binary_data,headers = self.headers, stream = True).text
-            },
-            'metadata': {},
-            'transient': {},
-        }
-
-        if not silent:
-            res = self.send_response(self.iopub_socket, 'display_data',stream_content)
-
-            return {'status': 'ok',
-            # The base class increments the execution count
-            'execution_count': self.execution_count,
-            'payload': [],
-            'user_expressions': {},
-            }
-
+    # called when the kernel is terminated
     def do_shutdown(self,restart):
         out = requests.get(MMT_BASE_URL + '/:' + MMT_SERVER_EXTENSION+'?quit',data = None,headers = self.headers, stream = True)
+
+    # wraps the message into the stream_content format
+    def get_stream_content(self,message):
+        return {
+        'data': {
+            'text/html': message
+        },
+        'metadata': {},
+        'transient': {},
+        }
 
 
 if __name__ == '__main__':
